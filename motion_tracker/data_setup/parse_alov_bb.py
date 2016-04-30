db870ae357e7ce629345ddf07f487e3980e16227
sallamander <ssall@alumni.nd.edu>
2016-04-30 16:38:00 -0600

    Finish up parse_alov_bb.py

--- motion_tracker/data_setup/parse_alov_bb.py
+++ motion_tracker/data_setup/parse_alov_bb.py
@@ -5,6 +5,7 @@ import os
 import itertools
 import shutil
 import pandas as pd
+import numpy as np
 
 def parse_file(bbox_dir, filename): 
     """Parse an individual `.ann` file and output the relevant elements.
@@ -45,16 +46,16 @@ def add_filepaths(parsed_df, input_dir, output_dir):
     parsed_df['filename'] = (parsed_df['filename']
             .apply(lambda filename: filename.replace('.ann', '')))
 
-    # Need to replace the filename ext. in the DataFrame with `.jpg` and 
-    # provide the full relative path point to what to move. 
+    # Need to replace the filename ext. in the DataFrame with `.jpg` and provide 
+    # the full relative path point to what to move. 
     parsed_df['input_filepath'] = (input_dir +   
         parsed_df['directory_path'] + '/' + 
         parsed_df['filename'] + '/' + parsed_df['vid_num'] 
         + '.jpg')
-    # Need to provide the full relative output path to point to 
-    # where to move. The `filename` is being used as part of 
-    # what to save so that images can be identified (there are 
-    # lot with the same number, so we need an identifying feature). 
+    # Need to provide the full relative output path to point to where to move. 
+    # The `filename` is being used as part of what to save so that images can be
+    # identified (there are lot with the same number, so we need an identifying
+    # feature). 
     parsed_df['output_filepath'] = (output_dir +
         parsed_df['filename'] + '_' + parsed_df['vid_num'] 
         + '.jpg')
@@ -70,44 +71,50 @@ def cp_files(filepaths_df):
     Args: 
     -----
         filepaths_df: pandas DataFrame
-            Holds 3 columns, each of which holds a piece that 
-            determines the input filepaths. We'll copy the files 
-            at these filepaths to the output_dir.
-    """
-    
+    """ 
+
     for input_fp, output_fp in zip(filepaths_df['input_filepath'], 
             filepaths_df['output_filepath']): 
         shutil.copy(input_fp, output_fp)
 
-def add_framepaths(save_df): 
-    """Add columns into the dataframe pointing to pairs of frames. 
+def calc_frame_pairs(end_df): 
+    """Add columns denoting the relevant info for the current and previous frame. 
 
-    For the image generator, we'll need to easily grab successive
-    frames, and have the bounding boxes for these frames readily 
-    available. To do this, we'll create columns for the filenames
-    for each frame, as well as columns for their bounding boxes. 
-    They will be differentiated by prefixes of current and previous. 
-
+    Each video has loads of frames, where every 5th (starting with the 1st) has 
+    a bounding box. We need to get information for subsequent frames in the same
+    row in the DataFrame, so it's easy for our generator to easily cycle through 
+    pairs. Here, we'll clean up the DataFrame and make sure that we end up with 
+    columns that distinguish between the current and previous frames filepaths as
+    well as bounding boxes. 
+    
     Args: 
     ----
+        start_df: pandas DataFrame
+
+    Returns: 
+    -------
         save_df: pandas DataFrame
     """
 
-    save_df = calc_frame_pairs(save_df)
+    filler_row = pd.DataFrame(np.zeros((1, end_df.shape[1])), columns=end_df.columns)
+    less_one_df = end_df[:-1]
+    lagged_df = pd.concat([filler_row, less_one_df], axis=0)
 
-def calc_frame_pairs(save_df): 
-    """Add columns denoting the filepaths of current and previous frames. 
+    lagged_cols = [col + '_start' for col in end_df.columns]
+    lagged_df.columns = lagged_cols
+    lagged_df.reset_index(inplace=True, drop=True)
 
-    Each video has loads of frames, where every 5th (starting with 
-    the 1st) has a bounding box. For every frame except the first 
-    and the last, we can simply add 5 to it. For the first, 
-    we need it to always be the previous frame since there's nothing
-    before it. For the last, we need it to always be the current, since 
-    there is nothing after it. 
-    """
-    pass
+    end_cols = [col + '_end' for col in end_df.columns]
+    end_df.columns = end_cols
+    merged_df = pd.concat([lagged_df, end_df], axis=1)
+    
+    max_frames_df = merged_df.groupby('filename_start')['frame_start'].max()
+    max_frames_df.name = 'max_frame'
+    temp_df = merged_df.join(max_frames_df, on='filename_start')
+    save_df = temp_df.query('max_frame != frame_start')
 
-
+    return save_df
+    
 if __name__ == '__main__': 
     input_dir = sys.argv[1]
     output_filepath = sys.argv[2]
@@ -123,17 +130,18 @@ if __name__ == '__main__':
     parsed_df = pd.concat(parse_file(bbox_dir, filename) for 
             filename in bbox_ann_filenames)
     parsed_df.columns = cols
+    parsed_df.reset_index(inplace=True, drop=True)
 
     frames_dir = input_dir + 'frames/'
     filepaths_df = add_filepaths(parsed_df, frames_dir, output_dir)
     cp_files(filepaths_df)
 
-    # Now that the copying is done, only a subset of the columns 
-    # are necessary. 
-    keep_cols = ['frame', 'x1', 'y1', 'x3', 'y3', 'jpg_filename', 
-            'filename']
-    save_df = filepaths_df[keep_cols]
-    '''
-    save_df = add_framepaths(save_df)
+    # Rename to match the project terminology and what's used with imagenet. 
+    filepaths_df.rename(columns={'x1':'x0', 'x3':'x1', 'y1':'y0', 'y3':'y1'}, inplace=True)
+    save_df = calc_frame_pairs(filepaths_df)
+    # Only keep what we'll need for the generator. 
+    keep_cols = ['x0_start', 'y0_start', 'x1_start', 'y1_start', 
+            'jpg_filename_start', 'x0_end', 'x1_end', 'y0_end', 'y1_end', 
+            'jpg_filename_end']
+    save_df = save_df[keep_cols]
     save_df.to_csv(output_filepath, index=False)
-    '''
